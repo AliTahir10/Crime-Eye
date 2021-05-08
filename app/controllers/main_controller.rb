ccaf4bb171c9a627b04bb403bf8f8ca2ed9251b6
Lee, Andy (UG - Computer Science) <al00952@surrey.ac.uk>
2021-05-08 17:41:47 +0100

    heroku news fix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,24 @@ class MainController < ApplicationController
   before_action :disable_flash_messages, only: [:home]
   before_action :require_user_is_logged_in, only: [:home]
 
-  def home; end
+  require 'news-api'
+
+  def home
+    newsApi = News.new(ENV['NEWSAPI_KEY'])
+    # @sources = newsApi.get_top_headlines(sources: "bbc-news")
+    @sources = newsApi.get_everything(q: "crime+arrested", sources: "bbc-news")
+    puts @sources.inspect
+
+    @sources.each do |article|
+      puts article.title
+      puts article.description
+      puts article.urlToImage
+      puts article.publishedAt
+      puts article.author
+    end
+
+
+  end
 
   def about; end
 end
