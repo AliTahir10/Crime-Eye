7190882db8da3b06d49bece07f043d21e2931510
Lee, Andy (UG - Computer Science) <al00952@surrey.ac.uk>
2021-05-08 18:35:17 +0100

    Integrated live news api to backend

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,15 +8,15 @@ class MainController < ApplicationController
     newsApi = News.new(ENV['NEWSAPI_KEY'])
     # @sources = newsApi.get_top_headlines(sources: "bbc-news")
     @sources = newsApi.get_everything(q: "crime+arrested", sources: "bbc-news")
-    puts @sources.inspect
-
-    @sources.each do |article|
-      puts article.title
-      puts article.description
-      puts article.urlToImage
-      puts article.publishedAt
-      puts article.author
-    end
+    # puts @sources.inspect
+    #
+    # @sources.each do |article|
+    #   puts article.title
+    #   puts article.description
+    #   puts article.urlToImage
+    #   puts article.publishedAt
+    #   puts article.author
+    # end
 
 
   end
